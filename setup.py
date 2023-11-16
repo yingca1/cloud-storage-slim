e5d982b68a6a07fa9db5145b1f01ad790eab916b
Ying Cai <i@caiying.me>
2023-11-16 08:27:54 +0000

    release v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cloud_storage_slim",
-    version="0.1",
+    version="0.1.1",
     author="Ying Cai",
     author_email="i@caiying.me",
     description="A lightweight cloud storage package for popular cloud storage services.",
